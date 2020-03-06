8a8c14ecffa50eb3675fee9c70100545838c535d
Sean Golden <S00154367@mail.itsligo.ie>
2020-03-06 00:21:24 +0000

    Init Commit

---
+++ MobileQuiz/Pages/About.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MobileQuiz.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+        public string GameCode { get; set; }
+
+        public void OnGet()
+        {            
+            Random r = new Random();
+            var x = r.Next(0, 1000000);
+            GameCode = x.ToString("000000");
+            
+        }
+    }
+}
---
+++ MobileQuiz/Pages/CreateProfile.cshtml.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MobileQuiz.Pages
+{
+    public class CreateProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void SelectIcon()
+        {
+
+            if()
+        }
+    }
+}
